14e4944b95df13dfb6a21c7e8e5a45e97ffc23c0
RAHUL KUMAR <rahulinbscit@live.com>
2018-11-23 16:31:41 +0530

    Added Taxation and Trademark Design and Controller.

---
+++ WMCbizWeb/Controllers/TaxationController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WMCbizWeb.Controllers
+{
+    public class TaxationController : Controller
+    {
+        // GET: Taxation
+        public ActionResult Computation()
+        {
+            return View();
+        }
+        public ActionResult FilingOf_ReturnsOf_Income()
+        {
+            return View();
+        }
+        public ActionResult FilingOfAppeals()
+        {
+            return View();
+        }
+        public ActionResult ClaimingRefunds()
+        {
+            return View();
+        }
+        public ActionResult TaxPlanning()
+        {
+            return View();
+        }
+        public ActionResult TaxManagement()
+        {
+            return View();
+        }
+        public ActionResult AvailingTax()
+        {
+            return View();
+        }
+        public ActionResult Concessions()
+        {
+            return View();
+        }
+        public ActionResult GST_Registration()
+        {
+            return View();
+        }
+        public ActionResult GST_Return()
+        {
+            return View();
+        }
+        
+    }
+}
