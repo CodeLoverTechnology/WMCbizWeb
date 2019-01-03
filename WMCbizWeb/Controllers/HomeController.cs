061b92aaaac406d2e7a369fff3a9b6031bb271b2
Indrajeet Singh <indrajeet.singh@codelovertechnology.com>
2019-01-03 17:45:24 +0530

    menu page done

--- WMCbizWeb/Controllers/HomeController.cs
+++ WMCbizWeb/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WMCbizWeb.Models;
 
 namespace WMCbizWeb.Controllers
 {
     public class HomeController : Controller
     {
+
+        private WMCEntities1 dbModel = new WMCEntities1();
         public ActionResult Index()
         {
             return View();
@@ -22,6 +25,29 @@ namespace WMCbizWeb.Controllers
 
         public ActionResult Contact()
         {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Enquiry(FormCollection frm)
+        {
+            string name = frm["txtname"].ToString();
+            string email = frm["txtemail"].ToString();
+            string Mobilenumber = frm["txtnumber"].ToString();
+
+            WMCUser ObjNew = new WMCUser();
+            ObjNew.Emailid = email;
+            ObjNew.MobileNo = Mobilenumber;
+            ObjNew.Name = name;
+            ObjNew.CreatedBy = email;
+            ObjNew.Createdate = DateTime.Now;
+            ObjNew.modifiedBy = email;
+            ObjNew.modifieddate = DateTime.Now;
+            ObjNew.Active = true;
+            dbModel.WMCUsers.Add(ObjNew);
+            dbModel.SaveChanges();
+
             ViewBag.Message = "Your contact page.";
 
             return View();
